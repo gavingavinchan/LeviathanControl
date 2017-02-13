129c7137517395492cc655aecae4fc4f219d1ac3
gavingavinchan <gavingavinchan@gmail.com>
2017-02-13 18:18:04 +0800

    Attiny program with only address change

---
+++ Attiny_program/AttinyControl/AttinyControl.ino
@@ -0,0 +1,36 @@
+#include "TinyWireS.h"                  // wrapper class for I2C slave routines
+#include <EEPROM.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  byte I2C_SLAVE_ADDR = 0;
+  EEPROM.get(0,I2C_SLAVE_ADDR);
+  TinyWireS.begin(I2C_SLAVE_ADDR);      // init I2C Slave mode
+  Blink(2); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (TinyWireS.available()){           // got I2C input!
+    byte byteCommand = TinyWireS.receive();     // get the byte from master
+    if(byteCommand == 0xCE) {
+      byte newAddress = TinyWireS.receive();
+      //Write value
+      EEPROM.put(0, 0x06);
+      Blink(1000);    
+      delay(1000);
+    } else if(byteCommand == 0x10) {
+      Blink(15);
+    }
+  }
+}
+
+void Blink(byte times){ // poor man's display
+  for (byte i=0; i< times; i++){
+    digitalWrite(1,HIGH);
+    delay (250);
+    digitalWrite(1,LOW);
+    delay (175);
+  }
+}
+
